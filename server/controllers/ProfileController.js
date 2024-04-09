018b4fc1d6c7f081f461eaa10df78d97a223318f
Sleeplessmen <124755138+Sleeplessmen@users.noreply.github.com>
2024-04-09 08:42:15 +0700

    add ProfileController.js and modify profile.js

---
+++ server/controllers/ProfileController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+
+class ProfileController {
+  
+    async getUserProfile(req, res, next) {
+        const user_id = req.params.user_id;
+  
+        try {
+            const user = await User.findById(user_id);
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+            res.json(user);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = new ProfileController();
--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,21 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const mysql_con = require('../config/db/mysql')
 
-router.get('/:username', async (req, res) => {
-    const username = req.params.username
+const profileController = require('../controllers/ProfileController')
 
-    const sqlQuery = `SELECT * `
-        + `FROM users `
-        + `WHERE user_id = ?`
-
-    mysql_con.query(sqlQuery, username, (error, results) => {
-        if (error) {
-            console.error('Error: ', error)
-            return res.status(500).json({ error: 'Internal Server Error' })
-        }
-        res.json(results)
-    })
-})
+router.get('/profile/:userId', profileController.getUserProfile);
 
 module.exports = router
