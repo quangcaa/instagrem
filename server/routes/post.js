550d7df0ff57a4dc10495254eb7ee2a8a68c2236
ng viet quang <nguyenvietquang.1601@gmail.com>
2024-04-11 18:14:41 +0700

    getPostById PostController

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { requireAuth } = require('../middlewares/auth')
+const { requireAuth, optionalAuth } = require('../middlewares/auth')
 const postController = require('../controllers/PostController')
+
+router.get('/:post_id', optionalAuth, postController.getPostById)
 
 router.post('/create', requireAuth, postController.createPost)
 router.put('/:id', requireAuth, postController.updatePost)
