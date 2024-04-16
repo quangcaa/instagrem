5beb3a3645e99f93fc6e1f418cd4ff5899397de5
Maou <nguyenvietquang.1601@gmail.com>
2024-04-16 11:24:36 +0700

    add activityController

---
+++ server/controllers/ActivityController.js
@@ -0,0 +1,60 @@
+const mysql_con = require('../config/db/mysql')
+
+class ActivityController {
+
+    // @route [GET] /activity
+    // @desc get user activity
+    // @access Private
+    async retrieveActivity(req, res) {
+        const receiver_id = req.userId
+
+        try {
+            const getActivityQuery = `
+                                        SELECT a.activity_type,
+                                               a.source_user_id,
+                                               a.post_id,
+                                               activity_title,
+                                               a.activity_message,
+                                               a.is_read,
+                                               a.created_at,
+                                               u.user_id,
+                                               u.username,
+                                               u.profile_image_url
+                                        FROM activities a
+                                        JOIN users u ON a.source_user_id = u.user_id
+                                        WHERE a.user_id = ?
+                                        ORDER BY created_at DESC
+                                        `
+            const [activityResult] = await mysql_con.promise().query(getActivityQuery, [receiver_id])
+
+            return res.status(200).json({ success: true, activities: activityResult })
+        } catch (error) {
+            console.error('Error: ', error)
+            return res.status(500).json({ error: 'Internal Server Error' })
+        }
+    }
+
+
+    // @route [PUT] /activity
+    // @desc mark activities as read
+    // @access Private
+    async readActivity(req, res) {
+        const user_id = req.userId
+
+        try {
+            const markAsReadQuery = `
+                                    UPDATE activities
+                                    SET is_read = 1
+                                    WHERE user_id = ?
+                                    `
+            await mysql_con.promise().query(markAsReadQuery, [user_id])
+
+            return res.status(200).json({ success: true, message: 'Read all activities ! ! !' })
+        } catch (error) {
+            console.error('Error: ', error)
+            return res.status(500).json({ error: 'Internal Server Error' })
+        }
+    }
+}
+
+module.exports = new ActivityController()
