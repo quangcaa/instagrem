348cb8aa0e2a6fe79abba20d606dcda0b0a3fba3
thaidzuicui <hthai313204@gmail.com>
2024-04-23 18:39:05 +0700

    .

---
+++ client/src/pages/PostPage.jsx
@@ -0,0 +1,85 @@
+import {
+  Avatar,
+  Flex,
+  Text,
+  Image,
+  Box,
+  Divider,
+  Button,
+} from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
+import Actions from "../components/Actions";
+import { useState } from "react";
+import Comment from "../components/Comment";
+
+const PostPage = () => {
+  const [liked, setLiked] = useState(false);
+  return (
+    <>
+      <Flex>
+        <Flex w={"full"} align={"center"} gap={3}>
+          <Avatar src="/zuck-avatar.png" size={"md"} name="Mark Zuck" />
+          <Flex>
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              markzuck
+            </Text>
+            <Image src="/verified.png" w={4} h={4} ml={4} />
+          </Flex>
+        </Flex>
+        <Flex gap={4} alignItems={"center"}>
+          <Text fontSize={"sm"} color={"gray.light"}>
+            1d
+          </Text>
+          <BsThreeDots />
+        </Flex>
+      </Flex>
+      <Text my={3}>Hello</Text>
+      <Box
+        borderRadius={6}
+        overflow={"hidden"}
+        border={"1px solid"}
+        borderColor={"gray.light"}
+      >
+        <Image src={"/post1.png"} w={"full"} />
+      </Box>
+
+      <Flex gap={3} my={3}>
+        <Actions liked={liked} setLiked={setLiked} />
+      </Flex>
+
+      <Flex gap={2} alignItems={"center"}>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          236 replies
+        </Text>
+        <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+        <Text color={"gray.light"} fontSize={"sm"}>
+          {200 + (liked ? 1 : 0)} likes
+        </Text>
+      </Flex>
+      <Divider my={4} />
+      <Flex justifyContent={"space-between"}>
+        <Flex gap={2} alignItems={"center"}>
+          <Text fontSize={"2xl"}>✌️</Text>
+          <Text color={"gray.light"}>Get the app to like, reply and post</Text>
+        </Flex>
+        <Button>Get</Button>
+      </Flex>
+      <Divider my={4} />
+      <Comment
+        comment="Looks great!"
+        createdAt="2d"
+        username="jakepaule"
+        userAvatar=""
+      />
+      <Comment
+        comment="Fuck!"
+        createdAt="10d"
+        username="loganpaule"
+        userAvatar=""
+      />
+      <Comment comment="Losing" createdAt="3d" username="haney" userAvatar="" />
+    </>
+  );
+};
+
+export default PostPage;
