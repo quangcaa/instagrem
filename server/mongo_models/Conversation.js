0de2accfa057f8ebd909d683eded5c3758302f01
ng viet quang <nguyenvietquang.1601@gmail.com>
2024-04-27 17:18:34 +0700

    fix Direct

--- server/mongo_models/Conversation.js
+++ server/mongo_models/Conversation.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const Conversation = new Schema(
     {
-        conversation_id: { type: Number, unique: true },
         participants: [{ type: String }],
         lastMessage_id: { type: mongoose.Schema.ObjectId, ref: 'messages' }
     },
@@ -12,8 +10,5 @@ const Conversation = new Schema(
         timestamps: true,
     }
 )
-
-// add plugin
-Conversation.plugin(AutoIncrement, { id: 'conversation_id_counter', inc_field: 'conversation_id' })
 
 module.exports = mongoose.model('conversations', Conversation)
--- server/mongo_models/Message.js
+++ server/mongo_models/Message.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema
 
 const Message = new Schema(
     {
-        conversation_id: { type: String, ref: 'conversations', required: true, },
+        conversation_id: { type: mongoose.Schema.Types.ObjectId, ref: 'conversations' },
         sender_id: { type: String, required: true },
         receiver_id: { type: String, required: true },
-        message: { type: String, required: true },
+        text: { type: String, required: true },
         reply_story_id: { type: String, ref: 'stories', default: null }
     },
     {
